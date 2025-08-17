19fedc6950f7465a1c6b403f9b7db0ba128ca0a0
Nehakkulkarni <nehakirankulkarni@gmail.com>
2025-08-17 21:33:36 +0530

    Area

--- C/Area.c
+++ C/Area.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
-
 int main()
 {
-    float area;
-    float radius;
-    float pi=3.14;
-    printf("Enter the radius of the circle: ");
-    scanf("%f",&radius);
-    area=pi*radius*radius;
-    printf("Area of circle is:%f",area);
+    float radius;float pi=3.14;
+    float side;
+    float length;float breath;
+    float base;float height;
+    //circle
+    printf("Enter the radius of the circle: ");scanf("%f",&radius);
+    printf("Area of circle is:%f \n",pi*radius*radius);
+    //square
+    printf("Enter the side of the square: ");scanf("%f",&side);
+    printf("Area of square is:%f \n",side*side);
+    //rectangle
+    printf("Enter the length of the rectangle:"); scanf("%f",&length);
+    printf("Enter the breath of the rectangle:");scanf("%f",&breath);
+    printf("Area of rectangle is:%f \n",length*breath);
+    //triangle
+    printf("Enter base of triangle:");scanf("%f",&base);
+    printf("Enter height of triangle:");scanf("%f",&height);
+    printf("Area of triangle is:%f \n",1.5*base*height);
 	return 0;
 }
