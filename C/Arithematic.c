379c1304dde68d4e9512951f1ad5ac74100c706a
Nehakkulkarni <nehakirankulkarni@gmail.com>
2025-08-17 20:40:21 +0530

    arithematic

--- C/Arithematic.c
+++ C/Arithematic.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d;
+    int a,b,c,d ,multi,div;
     a=14;b=4;c=a+b;d=a-b;
     printf("Addition is=%d \n",c );
     printf("Subtraction is=%d \n",d);
-     return 0;
+   
+	
+	multi=a*b;div=a/b;
+	printf("Multiplication is:%d \n",multi);
+	printf("Division is:%d \n ",div);
+	return 0;
+}
+
 }
